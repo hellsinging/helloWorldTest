d454b52c0c0ac3ec333d90664cff9c17b21f3416
Lyam Brownlow <lb00196@surrey.ac.uk>
2012-09-14 18:17:58 +0100

    added class

---
+++ helloWorld/src/com/surrey/lb00196/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.surrey.lb00196;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("hello World");
+		
+
+	}
+
+}
